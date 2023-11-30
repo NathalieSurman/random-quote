7e6e54eed17a03ea7f21f9aa3166f99bc5be7d34
Nathalie Surman <onepiecenat@hotmail.com>
2023-11-30 11:15:19 -0500

    styling and logic done

--- src/components/randomQuotes/RandomQuote.jsx
+++ src/components/randomQuotes/RandomQuote.jsx
@@ -1,32 +1,64 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './RandomQuote.css'
 import twitter_icon from '../assets/twitterxx.png'
 import reload from '../assets/reloadicon.png'
 
 const RandomQuote = () => {
 
+    let quotes = []; //This is where we will store all the quote data "using an API"
+    //-This function will get the quotes from an API
+    async function loadQuotes(){
+        const response = await fetch("https://type.fit/api/quotes");
+        quotes = await response.json();
+    }
+    
+    /*
+    useEffect(() => {
+        fetch("https://type.fit/api/quotes")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+        });
+
+    }, [])
+    */
+ 
+
+
     //-This will be the default quote
     const [quote, setQuote] = useState({
         text: "Be yourself; everyone else is already taken",
-        author: "― Oscar Wilde",
+        author: "Oscar Wilde",
     });
+
+    const random = () =>{
+        //-We wamt tp get a random quote
+        const select = quotes[Math.floor(Math.random()*quotes.length)];
+        setQuote(select);
+    }
+
+
+    const twitter = () =>{
+        window.open(`https://twitter.com/intent/tweet?text=${quote.text}  - ${quote.author.split(',')[0]}`);//-you can open a url 
+
+    }
+
+
+    //-we want to call the method
+    loadQuotes();
 
   return (
     <div className='container'>
-        <div className="quote">
-            {quote.text}
-        </div>
+        <div className="quote"> {quote.text}</div>
         <div>
-            <div className="line">
-                {}
-            </div>
+            <div className="line"></div>
             <div className="bottom">
-                <div className="author">
-                    {quote.author}
-                </div>
+                <div className="author">-  {quote.author.split(',')[0]}</div>
                 <div className="icons">
-                    <img src={reload} alt="reload icon" />
-                    <img src={twitter_icon} alt="twiiter icon logo" />
+                    <img src={reload} onClick={() => {random()}} alt="reload icon"  width="35px"/>
+                    <img src={twitter_icon} onClick={() => {twitter()}} alt="twiiter icon logo"  width="40px" />
                 </div>
             </div>
         </div>
